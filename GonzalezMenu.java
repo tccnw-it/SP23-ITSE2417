fdfeff0b298243ba207e160a0cf8b2764e671f48
Mikegonza <miguelangel.gonzalezmaur@my.tccd.edu>
2023-05-06 23:37:16 -0500

    ok

---
+++ GonzalezMenu.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class GonzalezMenu implements FinalProject
+{
+    public GonzalezMenu()
+    {
+        
+    }
+
+    public void begin() 
+    {
+        Scanner input = new Scanner(System.in);
+    
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+        int valz = year % 19;
+        int valy = year / 100;
+        int valx = year % 100;
+        int valw = valy / 4;
+        int valv = valy % 4;
+        int valu = (8 * valy + 13) / 25;
+        int valt = (19 * valz + valy - valw - valu + 15) % 30;
+        int vals = valx / 4;
+        int valr = valx % 4;
+        int valq = (valz + 11 * valt) / 319;
+        int valp = (2 * valv + 2 * vals - valr - valt + valq + 32) % 7;
+        int month = (valt - valq + valp + 90) / 25;
+        int day = (valt - valq + valp + month + 19) % 32;
+        System.out.printf("In " + year +"," + " Easter fell on " + month+"/"+day+"/"+year);
+        input.close();
+    }       
+}
